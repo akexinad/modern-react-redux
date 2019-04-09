ca9356b69468f0a72b69ee85b20796e5a8707c52
danny <danixeka@gmail.com>
2019-04-09 22:26:55 +1000

    image height not visible with refs

--- image_search/src/components/ImageCard.js
+++ image_search/src/components/ImageCard.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 
 export default class ImageCard extends Component {
-  render() {
+  constructor() {
+    super()
+    this.imageRef = React.createRef()
+  }
 
+  componentDidMount() {
+    console.log(this.imageRef.current.clientHeight)
+  }
+
+  render() {
     const { alt_description, urls } = this.props.image
 
     return (
       <div>
         <img
+          ref={ this.imageRef }
           src={ urls.regular }
           alt={ alt_description }
         />

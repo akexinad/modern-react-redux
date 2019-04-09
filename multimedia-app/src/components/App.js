87fbe654ead092ad525c9b919c1887056d35d3bb
danny <danixeka@gmail.com>
2019-04-10 06:52:56 +1000

    built basic structure

---
+++ multimedia-app/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import SearchBar from './SearchBar.js'
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+
+}

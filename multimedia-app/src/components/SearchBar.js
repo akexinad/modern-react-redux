f33712cd323d3fc58f7b747e46f166a101e7fbdf
danny <danixeka@gmail.com>
2019-04-10 06:57:35 +1000

    searchbar ready

--- multimedia-app/src/components/SearchBar.js
+++ multimedia-app/src/components/SearchBar.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+  state = {
+    searchTerm: '',
+  }
+
   render() {
     return (
-      <div>
-        Search Bar
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={ this.state.searchTerm }
+            onChange={ this._handleInputChange }
+          />
+        </form>
       </div>
     )
   }

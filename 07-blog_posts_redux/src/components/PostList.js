99b1e04c69e164a074baebbbb26c8b5598ea33c7
danny <danixeka@gmail.com>
2019-04-11 08:43:57 +1000

    created fetch user action and relevant reducer. Successfully rendering

--- 07-blog_posts_redux/src/components/PostList.js
+++ 07-blog_posts_redux/src/components/PostList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+
 import { fetchPosts } from '../actions'
+import UserHeader from './UserHeader.js'
 
 class PostList extends Component {
 
@@ -21,6 +23,7 @@ class PostList extends Component {
               <h2>{ post.title }</h2>
               <p>{ post.body }</p>
             </div>
+            <UserHeader userId={ post.userId }/>
           </div>
         </div>
       )

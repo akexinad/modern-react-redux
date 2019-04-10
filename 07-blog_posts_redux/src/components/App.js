03701367d328ac08cac5619783cbbbc33f0b3e81
danny <danixeka@gmail.com>
2019-04-10 16:41:36 +1000

    project prep

---
+++ 07-blog_posts_redux/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import PostList from './PostList.js'
+
+export default function App () {
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  )
+}
---
+++ 07-blog_posts_redux/src/components/PostList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class PostList extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        Post List
+      </div>
+    )
+  }
+}

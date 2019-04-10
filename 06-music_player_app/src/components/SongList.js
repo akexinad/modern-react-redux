3922b41bfe7800ac4af4abfb5366de78c475754a
danny <danixeka@gmail.com>
2019-04-10 14:08:31 +1000

    successfully recieving data from mapStateToProps

--- 06-music_player_app/src/components/SongList.js
+++ 06-music_player_app/src/components/SongList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 class SongList extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
         SongList
@@ -13,9 +14,11 @@ class SongList extends Component {
 
 // State holds our song list and selected song which is in our reducer
 const mapStateToProps = (state) => {
-
+  return {
+    songs: state.songs
+  }
 }
 
-export default connect()(SongList)
+export default connect(mapStateToProps)(SongList)
 // Connect is actually a react component in the world redux
 // The connect component gets out our list of songs from our Provider component [also a component in the world of redux]

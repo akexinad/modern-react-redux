e7b366e9fadb17993e5be4a89125e70eaabf9f77
danny <danixeka@gmail.com>
2019-04-11 10:31:15 +1000

    alternative .chain lodash method to fetch unique users. Much more elegant imo.

--- 07-blog_posts_redux/src/actions/index.js
+++ 07-blog_posts_redux/src/actions/index.js
@@ -2,12 +2,27 @@ import _ from 'lodash'
 import jsonPlaceholder from '../api/jsonPlaceholder.js'
 
 // NOTE: When we call an action creator from inside an action creator, we need to make sure that we dispatch the result of calling the action creator
+// export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+//   await dispatch(fetchPosts())
+//
+//   const uniqUserIds = _.uniq(_.map(getState().posts, 'userId'))
+//   uniqUserIds.forEach( (id) => dispatch(fetchUser(id)) )
+// }
+
+// OPTIONAL ACTION CREATOR
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts())
 
   const uniqUserIds = _.uniq(_.map(getState().posts, 'userId'))
   uniqUserIds.forEach( (id) => dispatch(fetchUser(id)) )
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach( (id) => dispatch(fetchUser(id)) )
+    .value() // value() EXECUTES the chaining. NECESSARY
 }
+
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts')

fcbb1ef6347460143f5d9e51af7bc53405433b5a
danny <danixeka@gmail.com>
2019-04-11 10:09:27 +1000

    return back to normal fetchUser action

--- 07-blog_posts_redux/src/actions/index.js
+++ 07-blog_posts_redux/src/actions/index.js
@@ -10,14 +10,23 @@ export const fetchPosts = () => async (dispatch) => {
   })
 }
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch)
-
-// The _ means that this is a PRIVATE FUNCTION
-const _fetchUser = _.memoize( async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${ id }`)
 
   dispatch({
     type: 'FETCH_USER',
     payload: response.data
   })
-})
+}
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch)
+//
+// The _ in _fetchUser means that this is a PRIVATE FUNCTION
+// const _fetchUser = _.memoize( async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${ id }`)
+//
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data
+//   })
+// })

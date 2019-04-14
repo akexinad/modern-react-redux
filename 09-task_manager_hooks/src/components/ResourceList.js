abe576ed089a7ddd37e6298e402b256de9e04bb3
danny <danixeka@gmail.com>
2019-04-14 18:35:43 +1000

    refactored ResourceList as functional component that utilises hooks

--- 09-task_manager_hooks/src/components/ResourceList.js
+++ 09-task_manager_hooks/src/components/ResourceList.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-export default class ResourceList extends React.Component {
-  state = {
-    resources: []
-  }
+export default function ResourceList() {
+  const [ resources, setResources ] = useState([])
 
-  async componentDidMount() {
+  const fetchResource = async () => {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${ this.props.resource }`)
 
     this.setState({
@@ -14,29 +12,17 @@ export default class ResourceList extends React.Component {
     })
   }
 
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${ this.props.resource }`)
-
-      this.setState({
-        resources: response.data
-      })
-    }
-  }
-
-  renderPosts() {
-    if (this.state.resources.length === 0) {
+  const renderPosts = () => {
+    if (resources.length === 0) {
       return <div>Loading...</div>
     }
 
     return (
       <div>
-        { this.state.resources.length }
+      { resources.length }
       </div>
     )
   }
 
-  render() {
-    return this.renderPosts()
-  }
+  return renderPosts()
 }

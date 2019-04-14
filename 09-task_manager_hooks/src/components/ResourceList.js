1c096f5f86a2a7b45107dd483d1043eb3bdccdb4
danny <danixeka@gmail.com>
2019-04-14 18:40:51 +1000

    lifecycles with useEffect

--- 09-task_manager_hooks/src/components/ResourceList.js
+++ 09-task_manager_hooks/src/components/ResourceList.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-export default function ResourceList() {
+export default function ResourceList({ resource }) {
   const [ resources, setResources ] = useState([])
 
-  const fetchResource = async () => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${ this.props.resource }`)
+  const fetchResource = async (resource) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${ resource }`)
 
-    this.setState({
-      resources: response.data
-    })
+    setResources(response.data)
   }
+
+  useEffect( () => {
+    fetchResource(resource)
+  }, [])
 
   const renderPosts = () => {
     if (resources.length === 0) {

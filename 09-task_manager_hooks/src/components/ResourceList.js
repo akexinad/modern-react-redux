e0d31ecb1df0ebf9ed8662e32318c2c1f1b51367
danny <danixeka@gmail.com>
2019-04-14 19:37:56 +1000

    put useResources in its own file

--- 09-task_manager_hooks/src/components/ResourceList.js
+++ 09-task_manager_hooks/src/components/ResourceList.js
@@ -1,20 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-const useResources = (resource) => {
-  const [ resources, setResources ] = useState([])
-
-  useEffect( () => {
-    (async (resource) => {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${ resource }`)
-
-      setResources(response.data)
-    })(resource)
-  },
-  [resource])
-
-  return resources
-}
+import useResources from './useResources.js'
 
 export default function ResourceList({ resource }) {
   const resources = useResources(resource)

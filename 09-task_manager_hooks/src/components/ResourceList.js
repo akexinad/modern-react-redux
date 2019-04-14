cfed3efd7bed8e86901bdb9da279e06a548662ed
danny <danixeka@gmail.com>
2019-04-14 19:09:06 +1000

    calling fetchResources within useEffect and then immediately invoking it. I.e. calling a function within a functuon and immediately invoking it. Allowed

--- 09-task_manager_hooks/src/components/ResourceList.js
+++ 09-task_manager_hooks/src/components/ResourceList.js
@@ -4,15 +4,14 @@ import axios from 'axios'
 export default function ResourceList({ resource }) {
   const [ resources, setResources ] = useState([])
 
-  const fetchResource = async (resource) => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${ resource }`)
-
-    setResources(response.data)
-  }
-
   useEffect( () => {
-    fetchResource(resource)
-  }, [resource])
+    (async (resource) => {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${ resource }`)
+
+      setResources(response.data)
+    })(resource)
+  },
+  [resource])
 
   const renderPosts = () => {
     if (resources.length === 0) {

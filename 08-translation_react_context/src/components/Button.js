d9e5d67eabf49f3334348bfb831cd3f05c21d0e7
danny <danixeka@gmail.com>
2019-04-14 11:55:49 +1000

    created and imported context type and sccssfuly logging in the console

--- 08-translation_react_context/src/components/Button.js
+++ 08-translation_react_context/src/components/Button.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import LanguageContext from '../contexts/LanguageContext.js'
+
 export default class Button extends React.Component {
+  // NOTE: contextType is a special variable name
+  static contextType = LanguageContext
+
   render() {
+    console.log(this.context);
     return (
       <button className="ui button primary">
         Submit
